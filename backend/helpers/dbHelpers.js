ee23251aeb0fa2d47321f98308614bd89656d08c
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-12 21:52:23 -0500

    change dbHelpers functions to better suit the app

---
+++ backend/helpers/dbHelpers.js
@@ -0,0 +1,51 @@
+module.exports = (db) => {
+  const getTeachers = () => {
+      const query = {
+          text: 'SELECT * FROM teachers',
+      };
+      return db
+          .query(query)
+          .then((result) => result.rows)
+          .catch((err) => err);
+  };
+
+  const getTeacherByEmail = email => {
+      const query = {
+          text: `SELECT * FROM teachers WHERE email = $1` ,
+          values: [email]
+      }
+      return db
+          .query(query)
+          .then(result => result.rows[0])
+          .catch((err) => err);
+  }
+
+  const addTeacher = (firstName, lastName, email, password) => {
+      const query = {
+          text: `INSERT INTO teachers (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
+          values: [firstName, lastName, email, password]
+      }
+      return db.query(query)
+          .then(result => result.rows[0])
+          .catch(err => err);
+  }
+
+  // const getAttendeePosts = () => {
+  //     const query = {
+  //         text: `SELECT attendee.id as attendee_id, first_name, last_name, email, posts.id as post_id, title, content
+  //     FROM users
+  //     INNER JOIN posts
+  //     ON users.id = posts.user_id`
+  //     }
+  //     return db.query(query)
+  //         .then(result => result.rows)
+  //         .catch(err => err);
+  // }
+
+  return {
+      getTeachers,
+      getTeachersByEmail,
+      addTeacher,
+      // getAttendeePosts
+  };
+};
