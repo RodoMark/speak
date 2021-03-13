fe7982d9012c6e6449a7e242b5dac1d4cc795cb2
thomasyee <thomas.w.yee@gmail.com>
2021-03-13 22:15:27 +0000

    routes setup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,79 @@
-var express = require('express');
-var router = express.Router();
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = function (router, database) {
+  //Post route for user registration
+  router.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, 12);
+    database
+      .userExists(req.body.email)
+      .then((boo) => {
+        if (!boo) {
+          res.send({ error: 'error' });
+          return;
+        }
+        database.addUser(req.body).then((user) => {
+          if (!user) {
+            res.send({ error: 'error' });
+            return;
+          }
+          req.session.userId = user.id;
+          res.send();
+        });
+      })
+      .catch((e) => res.send(e));
+  });
 
-module.exports = router;
+  //Helper function for correct password check
+  const login = function (email, password) {
+    return database.getUserWithEmail(email).then((user) => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  };
+  exports.login = login;
+
+  //Post route for user login
+  router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then((user) => {
+        if (!user) {
+          res.send({ error: 'error' });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({ user: { name: user.name, email: user.email, id: user.id } });
+      })
+      .catch((error) => res.send(error));
+  });
+
+  //Post route for user logout
+  router.post('/logout', (req, res) => {
+    req.session = null;
+    res.send();
+  });
+
+  // Fetches user information
+  router.get('/me', (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send();
+      return;
+    }
+
+    database
+      .getUserWithId(userId)
+      .then((user) => {
+        if (!user) {
+          res.send({ error: 'no user with that id' });
+          return;
+        }
+        res.send({ user: { name: user.name, email: user.email, id: userId } });
+      })
+      .catch((error) => res.send(error));
+  });
+
+  return router;
+};
