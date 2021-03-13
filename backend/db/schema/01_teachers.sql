5aab526c10dc34505e4df40a477b0429d5b568fc
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-12 21:33:06 -0500

    add rooms and attendees seeds

--- backend/db/schema/01_teachers.sql
+++ backend/db/schema/01_teachers.sql
@@ -3,6 +3,6 @@ CREATE TABLE teachers(
     id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
-    email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
