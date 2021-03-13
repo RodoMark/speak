69acb6418706cc82f1a1fe7a20e611ab322ec19c
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-13 15:22:26 -0500

    remove elements that don't need to be there

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,6 @@
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import {Navbar, Nav } from 'react-bootstrap'
 
-function Navigation () {
+function Navigation (props) {
   return (
 <Navbar bg="light" expand="lg">
   <Navbar.Brand href="#home">Parlar</Navbar.Brand>
@@ -8,19 +8,9 @@ function Navigation () {
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      <Nav.Link href="#profile">Profile</Nav.Link>
+      <Nav.Link href="#auth">Authenticate/Logout</Nav.Link>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
   </Navbar.Collapse>
 </Navbar>
   )
