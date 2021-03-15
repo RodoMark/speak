7e3f927a00788f4ecbb28faab800e376bdfcb948
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-15 19:00:31 -0400

    add id=footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import './styles/Footer.css'
 
 const Footer = () => {
   return(
-    <footer>
+    <footer id="footer">
       <h6>Made in 2021 by Thomas Yi, Eduardo Vásquez, Alon Rodovinsky</h6>
     </footer>
   ) 
