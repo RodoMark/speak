a4c272f3a076a54864c600f3626731b42a54f067
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-15 16:37:07 -0400

    story now reads on storybook

---
+++ client/src/stories/10-Footer.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+import Footer from "../components/Footer";
+
+export default {
+  title: 'Components/Footer',
+  component: Footer,
+}
+
+export const footer = () => <Footer primary>Button</Footer>;
+footer.storyName = 'I am the footer';
+
