0f670e6787d217d9108097fed4de4b4b4150beaa
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-15 18:47:15 -0400

    add Footer to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <h1>PARLAR</h1>
+      <Footer />
     </div>
   );
 }
