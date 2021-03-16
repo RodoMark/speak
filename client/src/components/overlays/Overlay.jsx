6a5c0af1ff4dbee371b43e10aac64fa160a3639a
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-16 15:53:04 -0400

    add dummy data for types

--- client/src/components/overlays/Overlay.jsx
+++ client/src/components/overlays/Overlay.jsx
@@ -2,15 +2,25 @@
 // import Await from "components/overlays/await.jsx";
 import Button from "../buttons/Button"
 
+const types = [
+	"calling",
+	"receiving",
+	"connecting",
+]
+
 const Overlay = (props) => {
 
 	///states: closeRoom confirmation, LeaveRoom COnfirmation, accepStageInvite, AwaitAnswer
 	
   return (
 		<div className="overlay">
-			<h2>{props.text}</h2>
-			<Button>Confirm</Button>
-			<Button>Cancel</Button>
+			<h2>{props.text}</h2><br />
+			<form>
+				<Button call confirm></Button>
+			</form>
+			<form>
+				<Button call reject></Button>
+			</form>
 		</div>
   )
 
