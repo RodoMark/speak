dd8559d9ac54223fb1835ff4097379b959bf5fba
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-03-16 18:06:34 -0400

    commit at 6

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className='App'>
       <Navigation />
-      <Camera title='video title' />
       <Footer />
     </div>
   );
--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,8 +1,8 @@
 // import all mayor components
 // import Stage from "../components/Stage/Stage.jsx"
 // import OverlayIndex from "../components/Overlays/OverlayIndex.jsx"
-import Dropdown from "./dropdown/Dropdown.jsx"
-import Chat from "./chat/Chat.jsx"
+import Dropdown from "./Dropdown/Dropdown.jsx"
+import Chat from "./Chat.jsx"
 import ExtraCompsBar from "../components/ExtraCompsBar/ExtraCompsBar.jsx"
 
 
