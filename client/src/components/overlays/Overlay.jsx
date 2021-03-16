53bc75cc80afee1d12d661cccfd0c07052e78c62
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-16 19:47:52 -0400

    add mode switches in Overlay

--- client/src/components/overlays/Overlay.jsx
+++ client/src/components/overlays/Overlay.jsx
@@ -5,14 +5,46 @@ import Calling from "../Calling"
 import Connecting from "../Connecting"
 import Receiving from "../Receiving"
 import Error from "../Error"
+
+import { useVisualMode } from "hooks/useVisualMode"
 		
-const types = {
-	calling: Calling,
-	receiving: Receiving,
-	connecting: Connecting,
-}   
+const modes = {
+	HIDDEN: "HIDDEN",
+	CALLING: "CALLING",
+	RECEIVING: "RECEIVING",
+	CONNECTING: "CONNECTING",
+	ERROR: "ERROR"
+}
+
+function confirm() {
+	transition(CONNECTING)
+	props.bookInterview(props.id, interview)
+		.then(() => transition(HIDDEN))
+		.catch((error) => transition (ERROR, true))
+}
+
+
+const { mode, transition, back } = useVisualMode(
+	props.type ? HIDDEN : ERROR
+);
 
 const Overlay = (props) => {
+	{mode === modes.HIDDEN && 
+		<div></div>
+	}
+
+	{mode === modes.CALLING && 
+		<Calling 
+			onClose={() => transition(HIDDEN)}
+		/>
+	}
+
+	{mode === modes.RECEIVING && 
+		<Receiving 
+			onConfirm={() => transition(CONNECTING)}
+		/>
+	}
+
 	///states: closeRoom confirmation, LeaveRoom COnfirmation, accepStageInvite, AwaitAnswer
 	const Display = types[props.type] || Error
 	return <Display />
