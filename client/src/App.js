4561ccfe9af10767d4edba5b5262b2e580f51a24
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-16 12:55:09 -0400

    pull request button components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.scss';
 import Navigation from './components/Navigation';
 import Button from './components/buttons/Button';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <h1>PARLAR</h1>
-      <Button danger></Button>
+      <Button call confirm></Button>
+      <Footer />
     </div>
   );
 }
