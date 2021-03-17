315574c7bc93417e741fcc9552f57779e32b111c
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-17 14:56:31 -0400

    add receivingCall and Recieve component to App

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,17 @@ import Navigation from './components/Navigation';
 import Overlay from './components/Overlays/Overlay';
 import Footer from './components/Footer';
 
+import useCameraData from "./hooks/useCameraData"
+
 import { OverlayContextProvider } from './hooks/useOverlayContext'
 
 function App() {
+  const { receivingCall } = useCameraData()
   
   return (
     <div className='App'>
       <Navigation />
+      { receivingCall && <Receiving />}
       <OverlayContextProvider>
         <Overlay />
       </OverlayContextProvider>
