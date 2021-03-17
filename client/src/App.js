6781be63bfd1bd9fd076ce6d213c49dbc1d069b3
Your Name <thomas.w.yee@gmail.com>
2021-03-17 19:44:19 -0400

    commit before pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import './App.scss';
 import Navigation from './components/Navigation';
-import Camera1 from './components/Camera1';
+import Stage from './components/Stage/Stage';
 import Footer from './components/Footer';
-import useCameraData from './hooks/useCameraData';
-import Chat from './components/chat/Chat';
 
 function App() {
-  const { io, message, handle } = useCameraData();
+  // const { io, message, handle } = useCameraData();
 
   return (
     <div className='App'>
       <Navigation />
       <h1>PARLAR</h1>
-      <Camera1 />
-      <Chat message={message} handle={handle} io={io} />
+      <Stage />
       <Footer />
     </div>
   );
