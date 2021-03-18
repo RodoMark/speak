8c95b1eb4780fccb7e5ba4c2275bfcfed0692485
Your Name <thomas.w.yee@gmail.com>
2021-03-18 15:20:01 -0400

    another fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 
 import './App.scss';
 import Navigation from './components/Navigation';
+import Stage from './components/Stage/Stage';
 import Footer from './components/Footer';
-import useCameraData from './hooks/useCameraData';
-import Chat from './components/chat/Chat';
 
 function App() {
-  const { io, message, handle } = useCameraData();
+  // const { io, message, handle } = useCameraData();
 
   return (
     <div className='App'>
