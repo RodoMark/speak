71ba8ae54e93acde115044552a64ff1d8a971f73
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-22 12:15:45 -0400

    add flex to messages

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react';
 
 export default function Message(props) {
   const { socket } = props;
-  const [msg, setMsg] = useState('.');
+  const [msg, setMsg] = useState('');
   socket.off('chat');
   socket.on('chat', (data) => {
     const message = data.message;
     const attendeeName = data.attendeeName;
     setMsg((prev) => (prev += `${attendeeName}: ${message},`));
   });
-  return <div class="messages">{msg ? msg.split(',').map((msg) => msg && <h4>{msg}</h4>) : null}</div>;
+  return <div class="messages">{msg ? msg.split(',').map((msg) => msg && <h4 className="message-text">{msg}</h4>) : null}</div>;
 }
