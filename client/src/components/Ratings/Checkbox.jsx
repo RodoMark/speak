308d988af99a3ccf694eaa0f98d6ca6fc6ad9790
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-22 22:58:04 -0400

    add Checkbox component with ratings state

---
+++ client/src/components/Ratings/Checkbox.jsx
@@ -0,0 +1,13 @@
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+
+const Checkbox = (props) => {
+  
+  const [ratings, setRatings] = useState(
+    [
+      {ethical: false}, 
+      {persuasive: false}, 
+      {wellInformed: false}]
+  )
+}
+
+export default Checkbox
